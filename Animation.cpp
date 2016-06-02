34cbb4f737d3a51825cb5adc92dff00eca0aba2d
Huskitch <huskitch@gmail.com>
2016-06-02 02:31:20 +0100

    Cleaned up animation class into crucible namespace, added a screen virtual object for switching between game screens.

--- Animation.cpp
+++ Animation.cpp
@@ -23,32 +23,32 @@
 
 #include "Animation.h"
 
-Animation::Animation() : m_texture(NULL)
+CRUCIBLE::Animation::Animation() : m_texture(NULL)
 {
 
 }
 
-void Animation::addFrame(sf::IntRect rect)
+void CRUCIBLE::Animation::addFrame(sf::IntRect rect)
 {
 	m_frames.push_back(rect);
 }
 
-void Animation::setSpriteSheet(const sf::Texture& texture)
+void CRUCIBLE::Animation::setSpriteSheet(const sf::Texture& texture)
 {
 	m_texture = &texture;
 }
 
-const sf::Texture* Animation::getSpriteSheet() const
+const sf::Texture* CRUCIBLE::Animation::getSpriteSheet() const
 {
 	return m_texture;
 }
 
-std::size_t Animation::getSize() const
+std::size_t CRUCIBLE::Animation::getSize() const
 {
 	return m_frames.size();
 }
 
-const sf::IntRect& Animation::getFrame(std::size_t n) const
+const sf::IntRect& CRUCIBLE::Animation::getFrame(std::size_t n) const
 {
 	return m_frames[n];
 }
